66afb37787fa1b761a7be16abb0ded88f207eaab
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2020-04-12 09:07:15 +0200

    improve line wrapping code

--- scripts_and_filters/wrap_lines.R
+++ scripts_and_filters/wrap_lines.R
@@ -1,16 +1,9 @@
 library(knitr)
-
 hook_output = knit_hooks$get('source')  #this is the output for code
 
 knit_hooks$set(source = function(x, options) {
-  n <- 70
-  
-  # this hook is used only when the linewidth option is not NULL
-  if (knitr::is_latex_output()) {
-    x = knitr:::split_lines(x)
-    # any lines wider than n should be wrapped
-    if (any(nchar(x) > n)) x = strwrap(x, width = n)
-    x = paste(x, collapse = '\n')
+  if (!is.null(n <- options$linewidth) & knitr::is_latex_output()) {
+    x <- strwrap(x, width = n, exdent = 4)
   }
   hook_output(x, options)
 })
