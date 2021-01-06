bcbeb5085f38e9e6776f3bfd743c5086765333de
ULRIK LYNGS <ulrik.lyngs@cs.ox.ac.uk>
2021-01-06 12:00:28 +0100

    use the soul package for correction highlighting that wraps lines

--- scripts_and_filters/correction_filter.lua
+++ scripts_and_filters/correction_filter.lua
@@ -1,6 +1,6 @@
 function Span (el)
   if el.classes[1] == "correction" then
-    table.insert(el.content, 1, pandoc.RawInline('latex', '\\colorbox[HTML]{CCCCFF}{'))
+    table.insert(el.content, 1, pandoc.RawInline('latex', '\\ctext[HTML]{CCCCFF}{'))
     table.insert(el.content, pandoc.RawInline('latex', '}'))
     end
   return el.content
