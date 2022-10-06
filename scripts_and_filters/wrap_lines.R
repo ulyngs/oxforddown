c5998096f710ebe97bb9060489803710d59d04ac
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2022-10-06 12:47:34 +0100

    rm old wrap lines function that didn't work

--- scripts_and_filters/wrap_lines.R
+++
@@ -1,9 +0,0 @@
-library(knitr)
-hook_output = knit_hooks$get('source')  #this is the output for code
-
-knit_hooks$set(source = function(x, options) {
-  if (!is.null(n <- options$linewidth) & knitr::is_latex_output()) {
-    x <- strwrap(x, width = n, exdent = 4)
-  }
-  hook_output(x, options)
-})
